562d9cc47b03e09001725202346cd529017d94c8
J Jesús Villanueva Jr <jesus1843@hotmail.com>
2023-02-25 09:21:35 -0600

    Public Test 5

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors')
 const mongoose = require('mongoose');
+const fetch = require('node-fetch');
 
 const server = express();
 
